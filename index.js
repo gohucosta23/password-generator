47a730119854e3df200d78cb17c93ade37ddfcb2
Hugo_Costa <gohucosta23@hotmail.com>
2020-02-27 23:52:03 -0500

    Adding conditon for length of password

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 
-var loCase =    "abcdefghijklmnopqrstuvwxyz";
+var loCase = "abcdefghijklmnopqrstuvwxyz";
 var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var nums = "0123456789";
 var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
@@ -18,25 +18,32 @@ var upNumsSpecial = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[\
 var allChar = "abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>0123456789?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var userLength = "";
 var pass = "";
-var button = document.addEventListener("click", genPassword());
-
-userLength = prompt('What is the length of your password?');
-var userChoices1 = confirm('Would you like your password to contain Lower Case Letters?');
-var userChoices2 = confirm('Would you like your password to contain Upper Case Letters?');
-var userChoices3 = confirm('Would you like your password to contain Numbers?');
-var userChoices4 = confirm('Would you like your password to contain Special Characters');
 
 
-    function genPassword(){
 
-    if (userChoices1 === true && userChoices2 === false && userChoices3 === false && userChoices4 === false) {       
+
+
+function genPassword() {
+
+    userLength = prompt('Please enter a length from 8 to 128 :');
+    if (userLength < 8 || userLength > 128) {
+        alert("Please , reload the page and enter from 8 to 128 characters");
+       
+    }
+    else {
+    var userChoices1 = confirm('Would you like your password to contain Lower Case Letters?');
+    var userChoices2 = confirm('Would you like your password to contain Upper Case Letters?');
+    var userChoices3 = confirm('Would you like your password to contain Numbers?');
+    var userChoices4 = confirm('Would you like your password to contain Special Characters');
+
+    if (userChoices1 === true && userChoices2 === false && userChoices3 === false && userChoices4 === false) {
         for (var i = 0; i < userLength; i++) {
             pass += loCase.charAt(Math.floor(Math.random() * Math.floor(loCase.length - 1)));
             
         }
     }
-    else if (userChoices2 === true && userChoices1 === false && userChoices3 === false && userChoices4 === false){
-        for(var i = 0; i < userLength; i++) {
+    else if (userChoices2 === true && userChoices1 === false && userChoices3 === false && userChoices4 === false) {
+        for (var i = 0; i < userLength; i++) {
             pass += upCase.charAt(Math.floor(Math.random() * Math.floor(upCase.length - 1)));
         }
     }
@@ -45,69 +52,71 @@ var userChoices4 = confirm('Would you like your password to contain Special Char
             pass += nums.charAt(Math.floor(Math.random() * Math.floor(nums.length - 1)));
         }
     }
-    else if (userChoices4 === true && userChoices1 === false && userChoices3 === false && userChoices2 === false){
-        for(var i = 0; i < userLength; i++) {
+    else if (userChoices4 === true && userChoices1 === false && userChoices3 === false && userChoices2 === false) {
+        for (var i = 0; i < userLength; i++) {
             pass += specialChar.charAt(Math.floor(Math.random() * Math.floor(specialChar.length - 1)));
         }
     }
     else if (userChoices1 === true && userChoices2 === true && userChoices3 === false && userChoices4 === false) {
-        for(var i = 0; i < userLength; i++) {
+        for (var i = 0; i < userLength; i++) {
             pass += loupCase.charAt(Math.floor(Math.random() * Math.floor(loupCase.length - 1)));
         }
     }
     else if (userChoices1 === true && userChoices3 === true && userChoices2 === false && userChoices4 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += loNums.charAt(Math.floor(Math.random() * Math.floor(loNums.length - 1)));
-            }
+        for (var i = 0; i < userLength; i++) {
+            pass += loNums.charAt(Math.floor(Math.random() * Math.floor(loNums.length - 1)));
         }
-        else if (userChoices1 === true && userChoices4 === true && userChoices3 === false && userChoices2 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += loSpecial.charAt(Math.floor(Math.random() * Math.floor(loSpecial.length - 1)));
-            }
+    }
+    else if (userChoices1 === true && userChoices4 === true && userChoices3 === false && userChoices2 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += loSpecial.charAt(Math.floor(Math.random() * Math.floor(loSpecial.length - 1)));
         }
-        else if (userChoices2 === true && userChoices3 === true && userChoices1 === false && userChoices2 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += upNums.charAt(Math.floor(Math.random() * Math.floor(upNums.length - 1)));
-            }
+    }
+    else if (userChoices2 === true && userChoices3 === true && userChoices1 === false && userChoices2 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += upNums.charAt(Math.floor(Math.random() * Math.floor(upNums.length - 1)));
         }
-        else if (userChoices2 === true && userChoices4 === true && userChoices1 === false && userChoices3 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += upSpecial.charAt(Math.floor(Math.random() * Math.floor(upSpecial.length - 1)));
-            }
+    }
+    else if (userChoices2 === true && userChoices4 === true && userChoices1 === false && userChoices3 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += upSpecial.charAt(Math.floor(Math.random() * Math.floor(upSpecial.length - 1)));
         }
-        else if (userChoices3 === true && userChoices4 === true && userChoices1 === false && userChoices2 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += numSpecial.charAt(Math.floor(Math.random() * Math.floor(numSpecial.length - 1)));
-            }
+    }
+    else if (userChoices3 === true && userChoices4 === true && userChoices1 === false && userChoices2 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += numSpecial.charAt(Math.floor(Math.random() * Math.floor(numSpecial.length - 1)));
         }
-        else if (userChoices1 === true && userChoices2 === true && userChoices3 === true && userChoices4 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += loUpNums.charAt(Math.floor(Math.random() * Math.floor(loUpNums.length - 1)));
-            }
+    }
+    else if (userChoices1 === true && userChoices2 === true && userChoices3 === true && userChoices4 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += loUpNums.charAt(Math.floor(Math.random() * Math.floor(loUpNums.length - 1)));
         }
-        else if (userChoices1 === true && userChoices2 === true && userChoices4 === true && userChoices3 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += loUpSpecial.charAt(Math.floor(Math.random() * Math.floor(loUpSpecial.length - 1)));
-            }
+    }
+    else if (userChoices1 === true && userChoices2 === true && userChoices4 === true && userChoices3 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += loUpSpecial.charAt(Math.floor(Math.random() * Math.floor(loUpSpecial.length - 1)));
         }
-        else if (userChoices1 === true && userChoices3 === true && userChoices4 === true && userChoices2 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += loNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(loNumsSpecial.length - 1)));
-            }
+    }
+    else if (userChoices1 === true && userChoices3 === true && userChoices4 === true && userChoices2 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += loNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(loNumsSpecial.length - 1)));
         }
-        else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === false) {
-            for(var i = 0; i < userLength; i++) {
-                pass += upNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(upNumsSpecial.length - 1)));
-            }
+    }
+    else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === false) {
+        for (var i = 0; i < userLength; i++) {
+            pass += upNumsSpecial.charAt(Math.floor(Math.random() * Math.floor(upNumsSpecial.length - 1)));
         }
-        else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === true) {
-            for(var i = 0; i < userLength; i++) {
-                pass += allChar.charAt(Math.floor(Math.random() * Math.floor(allChar.length - 1)));
-            }
+    }
+    else if (userChoices2 === true && userChoices3 === true && userChoices4 === true && userChoices1 === true) {
+        for (var i = 0; i < userLength; i++) {
+            pass += allChar.charAt(Math.floor(Math.random() * Math.floor(allChar.length - 1)));
         }
-    
-    document.getElementById("passOutput").value = pass;
+    }
 }
+    document.getElementById("passOutput").value = pass;
+    
+}
+
 
 
 
